1e7fbb6553f0d968328e71a5ca88f52e4c083875
Myaddib <Hotabich80@gmail.com>
2021-12-15 10:20:19 +0200

    Initial commit

---
+++ src/basic/java_codegym/Main.java
@@ -0,0 +1,61 @@
+package basic.java_codegym;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+// task 1
+        int x = 1;      // 	x equals 1
+        int y = x * 2;  // y equals 2
+        int z = 5 * y * y + 2 * y + 3; // z equals 20+4+3, which equals 27
+// task 2
+        int a = 5;      //	a equals 5
+        int b = 1;      //    b equals 1
+        int c = (a - b) * (a + b); // c equals 4*6, which equals 24
+// task 3
+        a = 64;         //a equals 64
+        b = a / 8;      //b equals 8
+        c = b / 4;      //c equals 2
+        int d = c * 3;  //d equals 6
+
+        String s = "Amigo"; //s contains "Amigo".
+        s = "123";          //s contains "123".
+        s = "123 + 456";    //s = "123 + 456";
+
+        s = "Amigo" + " is the best"; // contains "Amigo is the best".
+        s = ""; //contains an empty string – a string with no symbols at all.
+
+        // int x
+        x = 333;
+        s = "Amigo" + x;//  contains "Amigo333".
+
+        System.out.println("Amigo"); // Amigo
+        System.out.println("Ami"+"go");// Amigo
+
+        // String s
+        s = "Amigo";
+        System.out.println(s);// Amigo
+
+        s = "Am";
+        System.out.println(s+"igo");// Amigo
+
+        //Homework _ 1
+        System.out.println("Ellie is very smart");
+        System.out.println("Ellie is very smart");
+        System.out.println("Ellie is very smart");
+        System.out.println("Ellie is very smart");
+        System.out.println("Ellie is very smart");
+
+        //Homework _ 2
+        // int x,y;
+        x = 3126;
+        y = 8;
+        System.out.println(x-y);
+
+        //Homework _ 3
+        //String name;
+        //int age;
+        //String city;
+
+    }
+}
